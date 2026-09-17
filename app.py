001c97eb775ce6a9f9b9c349ede37f1128b7b5ed
jherney <jherney@users.noreply.github.com>
2026-09-17 18:15:36 -0300

    Move static assets to public/ and update Flask static folder

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from modules import MODULES
 
 
 def create_app(config_class=Config):
-    app = Flask(__name__, static_folder='static', template_folder='templates')
+    app = Flask(__name__, static_folder='public', template_folder='templates')
     app.config.from_object(config_class)
     init_db(app)
 
